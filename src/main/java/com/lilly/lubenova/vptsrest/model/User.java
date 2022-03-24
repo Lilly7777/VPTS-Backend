3ba7ca9c6370db0bc6f886dfaac29e00b5f38c0c
Liliya Lyubenova <lilly.lubenova@gmail.com>
2022-03-24 15:28:23 +0200

    [Update] Added paho client dependency and dateRegistered variable in user model

--- src/main/java/com/lilly/lubenova/vptsrest/model/User.java
+++ src/main/java/com/lilly/lubenova/vptsrest/model/User.java
@@ -1,23 +1,25 @@
 package com.lilly.lubenova.vptsrest.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.cloud.firestore.annotation.DocumentId;
 import com.google.cloud.firestore.annotation.PropertyName;
 import org.springframework.cloud.gcp.data.firestore.Document;
+
+import java.util.Date;
 
 @Document(collectionName = "users")
 public class User {
 
     @DocumentId
-    @PropertyName("_id")
     private String userId;
 
-    @PropertyName("full_name")
     private String fullName;
 
-    @PropertyName("phone_number")
     private String phoneNumber;
 
     private String location;
+
+    private Date dateRegistered = new Date();
 
     public User() {
     }
@@ -35,6 +37,8 @@ public class User {
         this.phoneNumber = phoneNumber;
     }
 
+    @PropertyName("_id")
+    @JsonProperty("_id")
     public String getUserId() {
         return userId;
     }
@@ -43,6 +47,8 @@ public class User {
         this.userId = userId;
     }
 
+    @PropertyName("full_name")
+    @JsonProperty("full_name")
     public String getFullName() {
         return fullName;
     }
@@ -51,6 +57,8 @@ public class User {
         this.fullName = fullName;
     }
 
+    @PropertyName("phone_number")
+    @JsonProperty("phone_number")
     public String getPhoneNumber() {
         return phoneNumber;
     }
@@ -59,11 +67,23 @@ public class User {
         this.phoneNumber = phoneNumber;
     }
 
+    @PropertyName("location")
+    @JsonProperty("location")
     public String getLocation() {
         return location;
     }
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    @PropertyName("date_registered")
+    @JsonProperty("date_registered")
+    public Date getDateRegistered() {
+        return dateRegistered;
+    }
+
+    public void setDateRegistered(Date dateRegistered) {
+        this.dateRegistered = dateRegistered;
     }
 }
