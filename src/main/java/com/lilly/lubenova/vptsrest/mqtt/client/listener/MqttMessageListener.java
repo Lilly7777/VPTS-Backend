21f2128b3cff99b44f663611d4d1dc537465d91c
Liliya Lyubenova <lilly.lubenova@gmail.com>
2022-03-26 16:36:36 +0200

    [Add] MQTT message listener

---
+++ src/main/java/com/lilly/lubenova/vptsrest/mqtt/client/listener/MqttMessageListener.java
@@ -0,0 +1,31 @@
+package com.lilly.lubenova.vptsrest.mqtt.client.listener;
+
+import com.lilly.lubenova.vptsrest.mqtt.client.impl.MqttSubscriberImpl;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Component("MqttMessageListener")
+public class MqttMessageListener implements Runnable {
+
+    private final MqttSubscriberImpl subscriber;
+
+    public MqttMessageListener(MqttSubscriberImpl subscriber) {
+        this.subscriber = subscriber;
+    }
+
+    private final AtomicBoolean run = new AtomicBoolean(true);
+
+    @PreDestroy
+    public void stop() {
+        run.set(false);
+    }
+
+    @Override
+    public void run() {
+        while (run.get()) {
+            subscriber.subscribeMessage("device/+");
+        }
+    }
+}
