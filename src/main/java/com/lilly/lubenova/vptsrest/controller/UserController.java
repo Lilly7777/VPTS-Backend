30ed1b81fc217e3da1e6cef2d9a52d1c5b90e3ba
Liliya Lyubenova <lilly.lubenova@gmail.com>
2022-03-26 20:03:35 +0200

    [Update] Added findAllByUserId dynamic query method in DeviceRepository, add HTTP PUT handling for User and HTTP GET handling for all user devices

--- src/main/java/com/lilly/lubenova/vptsrest/controller/UserController.java
+++ src/main/java/com/lilly/lubenova/vptsrest/controller/UserController.java
@@ -2,7 +2,9 @@ package com.lilly.lubenova.vptsrest.controller;
 
 import com.google.firebase.auth.FirebaseAuthException;
 import com.lilly.lubenova.vptsrest.authenticator.Authenticator;
+import com.lilly.lubenova.vptsrest.model.Device;
 import com.lilly.lubenova.vptsrest.model.User;
+import com.lilly.lubenova.vptsrest.repository.DeviceRepository;
 import com.lilly.lubenova.vptsrest.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,11 +12,17 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Objects;
+
 @RestController
 public class UserController {
 
     @Autowired
     protected UserRepository userRepository;
+
+    @Autowired
+    protected DeviceRepository deviceRepository;
 
     @Autowired
     protected Authenticator authenticator;
@@ -76,4 +84,49 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
         }
     }
+
+    @PutMapping("/user/{userId}")
+    public ResponseEntity<User> editUser(@RequestHeader(name = "Authorization", required = true) String authHeader, @PathVariable("userId") String userId, @RequestBody User editedUser) {
+        String uid;
+        try {
+            uid = authenticator.authentication(authHeader);
+        } catch (FirebaseAuthException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+        if (uid.equals(userId)) {
+            User user = userRepository.findById(uid).block();
+            if (user == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+            if (Objects.nonNull(editedUser.getFullName())) {
+                user.setFullName(editedUser.getFullName());
+            }
+            if (Objects.nonNull(editedUser.getLocation())) {
+                user.setLocation(editedUser.getLocation());
+            }
+            if (Objects.nonNull(editedUser.getPhoneNumber())) {
+                user.setPhoneNumber(editedUser.getPhoneNumber());
+            }
+            return ResponseEntity.status(HttpStatus.OK).body(userRepository.save(user).block());
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+        }
+    }
+
+    @GetMapping("/user/{userId}/devices")
+    public ResponseEntity<List<Device>> getUserDevices(@RequestHeader(name = "Authorization", required = true) String authHeader, @PathVariable("userId") String userId) {
+        String uid;
+        try {
+            uid = authenticator.authentication(authHeader);
+        } catch (FirebaseAuthException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+        if (uid.equals(userId)) {
+            return ResponseEntity.status(HttpStatus.OK).body(deviceRepository.findAllByUserId(userId).collectList().block());
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+        }
+
+    }
+
 }
--- src/main/java/com/lilly/lubenova/vptsrest/repository/DeviceRepository.java
+++ src/main/java/com/lilly/lubenova/vptsrest/repository/DeviceRepository.java
@@ -2,6 +2,8 @@ package com.lilly.lubenova.vptsrest.repository;
 
 import com.lilly.lubenova.vptsrest.model.Device;
 import org.springframework.cloud.gcp.data.firestore.FirestoreReactiveRepository;
+import reactor.core.publisher.Flux;
 
 public interface DeviceRepository extends FirestoreReactiveRepository<Device> {
+    Flux<Device> findAllByUserId(String userId);
 }
