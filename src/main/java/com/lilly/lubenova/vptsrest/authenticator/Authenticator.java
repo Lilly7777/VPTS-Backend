6a2474c9f9302bae734ae4fa7646b053fb365355
Liliya Lyubenova <lilly.lubenova@gmail.com>
2022-03-26 16:08:24 +0200

    [Update] Removed incorrect request header

--- src/main/java/com/lilly/lubenova/vptsrest/authenticator/Authenticator.java
+++ src/main/java/com/lilly/lubenova/vptsrest/authenticator/Authenticator.java
@@ -5,13 +5,12 @@ import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.context.annotation.ApplicationScope;
 
 @Component
 @ApplicationScope
 public class Authenticator {
-    public String authentication(@RequestHeader(name = "Authorization", required = true) String authHeader) throws FirebaseAuthException {
+    public String authentication(String authHeader) throws FirebaseAuthException {
         String token = StringUtils.substringAfter(authHeader, "Bearer ").trim();
         FirebaseToken decodedToken = null;
         decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
